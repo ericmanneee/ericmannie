37b7f57f8f77de58957d5df3bfc2b78407455544
EricMannie <iconicobes@gmail.com>
2023-06-01 13:59:25 +0100

    new commit

---
+++ learning/My Notes app/newNote.js
@@ -0,0 +1,30 @@
+const delEl = document.getElementById('delete')
+const containerEl = document.querySelector('.container')
+const input = document.querySelector('.textarea')
+const saveEl = document.getElementById('save')
+const filenameEl = document.getElementById('filename')
+
+
+getNote()
+function getNote(){
+    input.value = localStorage.getItem(filename)
+}
+
+function saveData(){
+    const data = input.value
+    const filename = filenameEl.value;
+    updateLocalStorage(filename, data)
+    
+}
+function  updateLocalStorage(filename, data){
+
+    localStorage.setItem(filename, data)
+
+    }
+
+
+saveEl.addEventListener('click', saveData)
+
+delEl.addEventListener('click', ()=>{
+    containerEl.remove()
+})
