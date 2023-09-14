91beb921cbb917394e3e2fd0f7f19339a79cc29a
EricMannie <iconicobes@gmail.com>
2023-09-14 15:23:04 +0100

    new commit

--- Banking-App/app.js
+++ Banking-App/app.js
@@ -35,11 +35,27 @@ function onLinkClick(event){
 window.onpopstate = () => updateRoute()
 updateRoute()
 
-const express = require ('express')
-const app = express()
-app.get('/', function(req,res){
-    res.sendFile(--__dirname + "/index.html")
-})
-app.listen(3000,function(){
-    console.log("server is running on localhost3000");
-})
+async function register(){
+    const registerForm = document.getElementById('registerForm')
+    const formData = new FormData(registerForm)
+    const data = Object.fromEntries(formData)
+    const jsonData = JSON.stringify(data)
+    const result = await createAccount(jsonData)
+    if(result.error){
+        return console.log('An Error Occured', result.error);
+    }
+    console.log('Account Created!', result);
+}
+async function createAccount(account){
+    try {
+        const response = await fetch('//localhost:3000/api/accounts', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: account
+        })
+        return await response.json()
+    } catch (error){
+        return{error: error.message || 'Unknown error'}
+    }
+    
+}
